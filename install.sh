c329809ce50d5c416781e96c3ad9518b8d40742e
Bob Aman <bob@sporkmonger.com>
2019-07-01 10:10:36 -0700

    Update install.sh with new dependency

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ go get github.com/kbinani/screenshot
 go get github.com/lxn/win
 go get golang.org/x/sys/windows
 go get github.com/atotto/clipboard
+go get github.com/miekg/dns
 pip3 install -r requirements.txt
 cd impacket
 python setup.py install
