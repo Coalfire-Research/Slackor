dca00a427bc7179ff5e49350441488f4fcced343
Bob Aman <bob@sporkmonger.com>
2019-07-01 21:53:18 -0700

    Use a glob that supports double-star.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ go get github.com/lxn/win
 go get golang.org/x/sys/windows
 go get github.com/atotto/clipboard
 go get github.com/miekg/dns
+go get github.com/bmatcuk/doublestar
 pip3 install -r requirements.txt
 cd impacket
 python setup.py install
