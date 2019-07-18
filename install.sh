71a4a3adaaf5411b625123d5946bdb7c182d2173
Bob Aman <bob@sporkmonger.com>
2019-07-18 00:52:22 +0000

    Tidying up and documenting

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 apt install golang xterm git python3-pip upx-ucl -y
+desired_install_location="$GOPATH/src/github.com/Coalfire-Research/Slackor"
+if [ $desired_install_location != `pwd` ]; then
+    >&2 echo "Slackor should be checked out within: $desired_install_location"
+    >&2 echo "Obtain with: go get github.com/Coalfire-Research/Slackor"
+fi
 go get github.com/atotto/clipboard
 go get github.com/bmatcuk/doublestar
 go get github.com/dustin/go-humanize
@@ -9,5 +14,4 @@ go get github.com/mattn/go-shellwords
 go get github.com/miekg/dns
 go get golang.org/x/sys/windows
 pip3 install -r requirements.txt
-cd impacket
-python setup.py install
+pushd impacket && python setup.py install && popd
